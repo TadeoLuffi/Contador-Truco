2832acd6508ae5c420049adda10b1829eac7c0af
Tadeo Luffi <131204961+TadeoLuffi@users.noreply.github.com>
2023-08-29 17:43:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// guarda el valor del contador en una variable
+
+// capturar el h1 del contador
+
+// Moatrarmel valor del contador actualizado
+function actualizar() {
+    h1
+}
+// Declarar funcion suma
+function sumar() {
+    if (contador ==30){
+        contador = 30
+    }
+    else{
+
+        contador = contador + 1
+    }
+    actualizar()
+}
+// Declarar funcion resta
+function restar() {
+    if (contador == 0) {
+        contador = 0
+        
+    }
+    contador = contador - 1 }
+// Declarar funcion reiniciar
